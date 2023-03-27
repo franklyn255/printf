160d5f720ca527181b2cb9f92a1bd127d6a22dfe
emmanuel007Uwadinmah <103425757+emmanuel007Uwadinmah@users.noreply.github.com>
2023-03-27 13:05:21 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 /**
  * _printf - formatted output conversion and print data.
  * @format: input string.
- *
+ *Collaborators of this project are Franklin and Emmanuel
  * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
